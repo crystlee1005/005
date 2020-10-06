5bb3ee21433f1a51f5ad898dfe695c4b13fa52ab
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-06 15:40:11 +0900

    실습2

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
 	int number;
 	printf(" 정수 하나를 입력하시오 :");
 	scanf("%i", &number);
-	 
+	  
 	 if(number > 0 )
-	 printf(" 양수입니다.\n");
+	 printf(" 절대값은 %i 입니다.\n", number);
 	 
 	 else if(number < 0)
-	 printf(" 음수입니다.\n");
+	 printf(" 절대값은 %i 입니다.\n", number*(-1));
 	 
 	 else
-	 printf(" 0 입니다.\n");
+	 printf(" 절대값은  0 입니다.\n");
 	  
 	
 	return 0;
