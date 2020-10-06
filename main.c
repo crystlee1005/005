b383ba35751d8eae33946de2908f6bac95141e52
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-06 15:34:35 +0900

    실습1

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ int main(int argc, char *argv[]) {
 	printf(" 정수 하나를 입력하시오 :");
 	scanf("%d", &number);
 	 
-	 if("%d>0", number )
-	 printf(" 양수입니다.");
+	 if(number > 0 )
+	 printf(" 양수입니다.\n");
 	 
-	 else if("%d<0", number)
-	 printf(" 음수입니다.");
+	 else if(number < 0)
+	 printf(" 음수입니다.\n");
 	 
 	 else
-	 printf(" 0 입니다.");
+	 printf(" 0 입니다.\n");
 	  
 	
 	return 0;
